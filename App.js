1d153e16e7b44cd080cd44a7a281a2bac5022d80
Hannes Cho <hoonsungcho11@gmail.com>
2021-12-19 23:04:53 +0100

    AppLoading

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { useState } from "react";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require("./assets/logo.png"),
+      "https://imgr.search.brave.com/8-0zkm9oMHpp5-TaumW4M0ZDCM6xCGN8fQwwt1ChKbQ/fit/1200/1200/ce/1/aHR0cHM6Ly93d3cu/cG5na2V5LmNvbS9w/bmcvZnVsbC80NTMt/NDUzNjA5MF9jb2Zm/ZWUtY3VwLWljb24t/cG5nLWhvdC1jb2Zm/ZWUtdmVjdG9yLXBu/Zy5wbmc",
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all(...fontPromises, ...imagePromises);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
